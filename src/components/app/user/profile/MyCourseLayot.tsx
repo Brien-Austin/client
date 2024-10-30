09339bab2ddfda9ef628961a8626a5860fd8deaf
Brien-Austin <brienaustinclayton@gmail.com>
2024-10-30 14:11:44 +0530

    added back router

--- src/components/app/user/profile/MyCourseLayot.tsx
+++ src/components/app/user/profile/MyCourseLayot.tsx
@@ -1,15 +1,17 @@
 import { ChevronLeft } from "lucide-react"
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
 const MyCourseLayot = ({children} : {
     children : React.ReactNode
 }) => {
+    const router = useNavigate()
   return (
 <aside>
     <nav className=" border-b border-neutral-50 shadow-sm bg-white z-50 w-full top-0 left-0  ">
        <div className="p-4 flex items-center space-x-4">
 
-        <div className="flex  items-center justify-center px-3 py-2 border rounded-lg text-purple-600">
+        <div onClick={()=>{router(-1)}} className="flex  items-center justify-center px-3 py-2 border rounded-lg text-purple-600">
             <ChevronLeft/>
         </div>
         <h1 className="text-neutral-700 font-medium  text-lg">Enrolled Courses</h1>
