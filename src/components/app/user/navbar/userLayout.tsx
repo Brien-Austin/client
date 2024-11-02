369e882b7a02f4de8c1cc70218288fd0497aed0f
Brien-Austin <brienaustinclayton@gmail.com>
2024-11-02 19:37:12 +0530

    error fixed

--- src/components/app/user/navbar/userLayout.tsx
+++ src/components/app/user/navbar/userLayout.tsx
@@ -1,57 +1,49 @@
-import React, { useEffect } from 'react'
-import NavBar from './navbar'
+import React, { useEffect } from 'react';
+import NavBar from './navbar';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-
-import { useLocation, useNavigate } from 'react-router-dom'
-
-
-const UserLayout = ({children} : {children : React.ReactNode}) => {
-
- 
-
+const UserLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useNavigate();
   const location = useLocation();
 
- 
-useEffect(()=>{
-  setTimeout(()=>{
- 
-    const queryParams = new URLSearchParams(location.search);
-    const accessToken = queryParams.get('accessToken');
-    const refreshToken = queryParams.get('refreshToken');
-  
-    if (accessToken && refreshToken) {
-      // Store tokens in local storage
-      localStorage.setItem('userAccessToken', accessToken);
-      localStorage.setItem('userRefreshToken', refreshToken);
-  
-      // Remove the tokens from the URL
-      const newUrl = window.location.pathname; // Only keep the path without query parameters
-      window.history.replaceState({}, document.title, newUrl);
-
-   
-  }},1000)
+  useEffect(() => {
     
-},[location.search])
-  
-  const at = localStorage.getItem('userAccessToken')
-  useEffect(()=>{
-    if(!at){
-      router("/onboard")
+    const timeoutId = setTimeout(() => {
+      const queryParams = new URLSearchParams(location.search);
+      const accessToken = queryParams.get('accessToken');
+      const refreshToken = queryParams.get('refreshToken');
+
+      if (accessToken && refreshToken) {
+
+        localStorage.setItem('userAccessToken', accessToken);
+        localStorage.setItem('userRefreshToken', refreshToken);
+
+       
+        const newUrl = window.location.pathname; 
+        window.history.replaceState({}, document.title, newUrl);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timeoutId); 
+  }, [location.search]);
+
+  useEffect(() => {
+    const at = localStorage.getItem('userAccessToken');
+
+    
+    if (!at) {
+      router('/onboard');
     }
-    
-  },[at,router])
-  
-  return (
-    <main className='h-screen w-full ' >
-        <NavBar/>
-  
-        <div className="mt-5 lg:px-10 sm:px-5 ">
-        {children}
-        </div>
-        
-    </main>
-  )
-}
+  }, [router]); 
 
-export default UserLayout
+  return (
+    <main className='h-screen w-full'>
+      <NavBar />
+      <div className="mt-5 lg:px-10 sm:px-5">
+        {children}
+      </div>
+    </main>
+  );
+};
+
+export default UserLayout;
